d4ba33376da76c1e4d69938efc632072a7ec3e87
ABHISHEK <121852441+vyomic@users.noreply.github.com>
2023-10-16 10:40:16 +0530

    Update searchbar.js

--- js/searchbar.js
+++ js/searchbar.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
         // Prevent the default form submission behavior
         event.preventDefault();
         var inputText = $('#urlinput').val();
-        var query = "https://www.google.com/search?q="+inputText;
+        var query = "https://www.bing.com/search?q="+inputText;
         //var newHtml= "<iframe src='"+query+"' title='Your Search Results'></iframe>";
-        var newHtml= "<iframe src='https://www.youtube.com/' title='Your Search Results'></iframe>";
+        var newHtml= "<iframe src='"+query+"' title='Your Search Results' width='100%' height='580px'></iframe>";
         console.log(query);
       $("#mainbody").html(newHtml);
       
