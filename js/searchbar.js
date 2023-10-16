440465e5f02638b1b3fa173cbd1b40d5a95db4ac
ABHISHEK <121852441+vyomic@users.noreply.github.com>
2023-10-16 11:00:17 +0530

    Update searchbar.js

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
-    $('.urlbox').on('submit', function(event) {
-        // Prevent the default form submission behavior
-        event.preventDefault();
-        var inputText = $('#urlinput').val();
-        var query = "https://www.bing.com/search?q="+inputText;
-        //var newHtml= "<iframe src='"+query+"' title='Your Search Results'></iframe>";
-        var newHtml= "<iframe src='"+query+"' title='Your Search Results' width='100%' height='580px'></iframe>";
-        console.log(query);
-      $("#mainbody").html(newHtml);
-      
-    });
+  
+  $('#urlinput').focus(function() {
+    $('#urlinput').attr("placeholder", "Search on bing...");
+  });
+
+  $('#urlinput').blur(function() {
+    $('#urlinput').attr("placeholder", "https://vyomic.github.io/about");
+  });
+
+
+
+  $('.urlbox').on('submit', function(event) {
+      // Prevent the default form submission behavior
+      event.preventDefault();
+      var inputText = $('#urlinput').val();
+      var query = "https://www.bing.com/search?q="+inputText;
+      //var newHtml= "<iframe src='"+query+"' title='Your Search Results'></iframe>";
+      var newHtml=  "<iframe src='"+query+"' title='Your Search Results' width='100%' height='100%'></iframe>";
+      console.log(query);
+    $("#mainbody").html(newHtml);
     
+  });
+  
 });
